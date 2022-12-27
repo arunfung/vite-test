42cdb96d28d6e460acc6e4421c217a03cd94cc6a
Arun Fung <arunfung@gmail.com>
2022-12-27 22:45:04 +0800

    配置提示

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-export default {
-    // optimizeDeps: {
-    //     exclude: ["lodash-es"], // 当遇到 lodash-es 这个依赖不进行依赖预构建
-    // }
+// import { defineConfig} from "vite"; 1、可以通过导入方式来增加配置提示
+
+/**  @type import("vite").UserConfig */ // 2、注释的方式增加配置提示
+const viteConfig = {
+    optimizeDeps: {
+        exclude: [], // 将指定的依赖不进行依赖预构建
+    }
 }
+
+export default viteConfig;
