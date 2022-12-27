394d6765898f402833f26b398aad71cf98d45b26
Arun Fung <arunfung@gmail.com>
2022-12-27 23:37:13 +0800

    服务端区分变量

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import viteBaseConfig from "./vite.base.config";
 import viteDevConfig from "./vite.dev.config";
 import viteProdConfig from "./vite.prod.config";
@@ -13,7 +13,8 @@ const envResolver = {
         return ({...viteBaseConfig, ...viteDevConfig})
     },
 }
-export default defineConfig(({command}) => {
-    console.log("command", command);
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), "")
+    console.log("env", env.APP_KEY);
     return envResolver[command]();
 })
